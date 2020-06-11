432089fbced64ba8f6057017d879aa995e91f4e8
SoerenToennesen <sorentoennesen@gmail.com>
2020-06-11 21:44:37 +0200

    Initial commit

---
+++ ClientModule/src/ClientMain.java
@@ -0,0 +1,183 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ClientMain {
+    private final String serverName;
+    private final int serverPort;
+    private Socket socket;
+    private OutputStream serverOut;
+    private InputStream serverIn;
+    private BufferedReader bufferedIn;
+
+    private ArrayList<UserStatusListener> userStatusListeners = new ArrayList<>();
+    private ArrayList<MessageListener> messageListeners = new ArrayList<>();
+
+    public ClientMain(String serverName, int serverPort) {
+        this.serverName = serverName;
+        this.serverPort = serverPort;
+    }
+    public static void main(String[] args) throws IOException {
+        ClientMain client = new ClientMain("localhost", 8818);
+        client.addUserStatusListener(new UserStatusListener() {
+            @Override
+            public void online(String login) {
+                System.out.println("ONLINE: " + login);
+            }
+
+            @Override
+            public void offline(String login) {
+                System.out.println("OFFLINE: " + login);
+            }
+        });
+
+        client.addMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(String fromLogin, String messageBody) {
+                System.out.println(fromLogin);
+                System.out.println("Message from " + fromLogin + ": " + messageBody);
+            }
+        });
+
+        if (!client.connect()) {
+            System.err.println("Connection failed right here...");
+        } else {
+            System.out.println("Connection successful right here...");
+            if (client.login("guest", "guest")) {
+                System.out.println("Login successful right here");
+                client.message("jim", "Hello, how are you?");
+            } else {
+                System.err.println("Login failed");
+            }
+            //client.logoff();
+        }
+    }
+
+    private void message(String sendTo, String messageBody) throws IOException {
+        String cmd = "message " + sendTo + " " + messageBody + "\r\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    private void logoff() throws IOException {
+
+        String cmd = "logoff\r\n ";
+        serverOut.write(cmd.getBytes());
+
+    }
+
+    private boolean login(String login, String password) throws IOException {
+
+        String cmd = "login " + login + " " + password + "\r\n";
+        serverOut.write(cmd.getBytes());
+
+        String response = bufferedIn.readLine();
+        System.out.println("Response line: " + response);
+
+        if ("Login successful".equalsIgnoreCase(response)) {
+            startMessageReader();
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private void startMessageReader() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                readMessageLoop();
+            }
+        };
+        t.start();
+    }
+
+    private void readMessageLoop() {
+        try {
+            String line;
+            while ((line = bufferedIn.readLine()) != null) {
+                String[] tokens = StringUtils.split(line);
+                if (tokens != null && tokens.length > 0) {
+                    String cmd = tokens[0];
+                    if ("online".equalsIgnoreCase(cmd)) {
+                        handleOnline(tokens);
+                    } else if ("offline".equalsIgnoreCase(cmd)) {
+                        handleOffline(tokens);
+                    } else if ("message".equalsIgnoreCase(cmd)) {
+                        String[] tokensMsg = StringUtils.split(line, null, 3);
+                        handleMessage(tokensMsg);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    private void handleMessage(String[] tokensMsg) {
+
+        String login = tokensMsg[1];
+        String messageBody = tokensMsg[2];
+        System.out.println(login + " YO");
+        System.out.println(messageBody + " DUDE");
+
+        for (MessageListener listener : messageListeners) {
+            listener.onMessage(login, messageBody);
+        }
+
+    }
+
+    private void handleOffline(String[] tokens) {
+        String login = tokens[1];
+        for (UserStatusListener listener : userStatusListeners) {
+            listener.offline(login);
+        }
+    }
+
+    private void handleOnline(String[] tokens) {
+        String login = tokens[1];
+        for (UserStatusListener listener : userStatusListeners) {
+            listener.online(login);
+        }
+    }
+
+    private boolean connect() throws IOException {
+        try {
+            this.socket = new Socket(serverName, serverPort);
+            System.out.println("Client port is " + socket.getLocalPort());
+            this.serverOut = socket.getOutputStream();
+            this.serverIn = socket.getInputStream();
+            this.bufferedIn = new BufferedReader(new InputStreamReader(serverIn));
+            //Thread inputListener = new Thread(new ResponseListener(socket));
+            //inputListener.start();
+            //socket.close();
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //socket.close();
+        return false;
+
+    }
+
+    public void addUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.add(listener);
+    }
+    public void removeUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.remove(listener);
+    }
+    public void addMessageListener(MessageListener listener) {
+        messageListeners.add(listener);
+    }
+    public void removeMessageListener(MessageListener listener) {
+        messageListeners.remove(listener);
+    }
+
+}
---
+++ ServerModule/src/ServerWorker.java
@@ -0,0 +1,183 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+
+
+public class ServerWorker extends Thread {
+    private final Socket clientSocket;
+    private final Server server;
+    private String login = null;
+    private OutputStream outputStream;
+    private HashSet<String> topicSet = new HashSet<>();
+
+    public ServerWorker(Server server, Socket clientSocket) {
+        this.server = server;
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            handleClientSocket();
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void handleClientSocket() throws IOException, InterruptedException {
+        InputStream inputStream = clientSocket.getInputStream();
+        this.outputStream = clientSocket.getOutputStream();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] tokens = StringUtils.split(line);
+            if (tokens != null && tokens.length > 0) {
+                String cmd = tokens[0];
+                if ("logoff".equals(cmd) || "quit".equalsIgnoreCase(line)) {
+                    handleLogoff();
+                    break;
+                } else if ("login".equalsIgnoreCase(cmd)) {
+                    handleLogin(outputStream, tokens);
+                } else if ("message".equalsIgnoreCase(cmd)) {
+                    String[] tokensMsg = StringUtils.split(line, null, 3);
+                    handleMessage(tokens);
+                } else if ("join".equalsIgnoreCase(cmd)) {
+                    handleJoin(tokens);
+                } else if ("leave".equalsIgnoreCase(cmd)) {
+                    handleLeave(tokens);
+                }
+                else {
+                    String msg = "Unknown command: " + cmd + "\r\n";
+                    outputStream.write(msg.getBytes());
+                }
+            }
+        }
+
+        clientSocket.close();
+    }
+
+    private void handleLeave(String[] tokens) throws IOException {
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            String msg = "Left chatroom " + topic + "\r\n";
+            outputStream.write(msg.getBytes());
+            topicSet.remove(topic);
+        }
+    }
+
+    public boolean isMemberOfTopic(String topic) {
+        return topicSet.contains(topic);
+    }
+
+    private void handleJoin(String[] tokens) throws IOException {
+
+        if (tokens.length > 1) {
+            String topic = tokens[1];
+            String msg = "Joined chatroom " + topic + "\r\n";
+            outputStream.write(msg.getBytes());
+            topicSet.add(topic);
+        }
+
+    }
+
+    // Format: "Message" "login" body...
+    // Format: "Message" "#topic" body...
+    private void handleMessage(String[] tokens) throws IOException {
+
+        String sendTo = tokens[1];
+        String body = tokens[2];
+
+        boolean isTopic = sendTo.charAt(0) == '#';
+
+        List<ServerWorker> workerList = server.getWorkerList();
+        for (ServerWorker worker: workerList) {
+
+            if (isTopic) {
+                if (worker.isMemberOfTopic(sendTo)) {
+                    String outMsg = "Message from " + login + " to " + sendTo + ": " + body + "\r\n";
+                    worker.send(outMsg);
+                }
+            } else {
+                if (sendTo.equalsIgnoreCase(worker.getLogin())) {
+                    String outMsg = "Message from " + login + ": " + body + "\r\n";
+                    worker.send(outMsg);
+                }
+            }
+        }
+
+    }
+
+    private void handleLogoff() throws IOException {
+        server.removeWorker(this);
+        List<ServerWorker> workerList = server.getWorkerList();
+        // Send other online users current user's status
+        String onlineMsg = "Offline " + login + "\r\n";
+        for (ServerWorker worker: workerList) {
+            if (!login.equals(worker.getLogin())) {
+                worker.send(onlineMsg);
+            }
+        }
+        clientSocket.close();
+
+    }
+
+
+    public String getLogin() {
+        return login;
+    }
+
+    private void handleLogin(OutputStream outputStream, String[] tokens) throws IOException {
+        if (tokens.length == 3) {
+            String login = tokens[1];
+            String password = tokens[2];
+
+            if ((login.equals("guest") && password.equals("guest"))
+                    || (login.equals("jim") && password.equals("jim"))) {
+                String msg = "Login successful\r\n";
+                outputStream.write(msg.getBytes());
+                this.login = login;
+                System.out.println("User logged in successfully: " + login);
+
+
+                List<ServerWorker> workerList = server.getWorkerList();
+
+                // Send current user all other online logins
+                for (ServerWorker worker: workerList) {
+                    if (worker.getLogin() != null) {
+                        if (!login.equals(worker.getLogin())) {
+                            String msg2 = "Online " + worker.getLogin() + "\r\n";
+                            send(msg2);
+                        }
+                    }
+                }
+                // Send other online users current user's status
+                String onlineMsg = "Online " + login + "\r\n";
+                for (ServerWorker worker: workerList) {
+                    if (!login.equals(worker.getLogin())) {
+                        worker.send(onlineMsg);
+                    }
+                }
+
+            } else {
+                String msg = "Unsuccessful login\r\n";
+                outputStream.write(msg.getBytes());
+                System.err.println("Login failed for " + login);
+            }
+        }
+    }
+
+    private void send(String onlineMsg) throws IOException {
+        if (login != null) {
+            outputStream.write(onlineMsg.getBytes());
+        }
+
+
+
+    }
+
+}
